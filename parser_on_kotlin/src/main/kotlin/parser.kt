8cfd43779aab255124e97799541e0a8b5690d315
Newbilius <newbilius@gmail.com>
2018-01-04 23:02:28 +0500

    Добавил кондовую поддержку https

--- parser_on_kotlin/src/main/kotlin/parser.kt
+++ parser_on_kotlin/src/main/kotlin/parser.kt
@@ -3,16 +3,55 @@ import com.google.gson.Gson
 import org.jsoup.Jsoup
 import java.io.FileWriter
 import java.io.PrintWriter
+import java.security.KeyManagementException
+import java.security.NoSuchAlgorithmException
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier
+import javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSession
+import javax.net.ssl.X509TrustManager
 
 class Game(val Name: String, val Genre: String, val Url: String, val Year: String)
 
+//todo очень нехорошо принимать все подряд сертификаты, но я пока не готов написать нормальный код в этом месте >_>
+@Throws(KeyManagementException::class, NoSuchAlgorithmException::class)
+fun enableSSLSocket() {
+    setDefaultHostnameVerifier(object : HostnameVerifier {
+        override fun verify(hostname: String, session: SSLSession): Boolean {
+            return true
+        }
+    })
+
+    val context = SSLContext.getInstance("TLS")
+    context.init(null, arrayOf<X509TrustManager>(elements = object : X509TrustManager {
+        override fun getAcceptedIssuers(): Array<X509Certificate?> {
+            return arrayOfNulls<X509Certificate?>(0)
+        }
+
+        @Throws(CertificateException::class)
+        override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {
+        }
+
+        @Throws(CertificateException::class)
+        override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {
+        }
+    }), SecureRandom())
+    setDefaultSSLSocketFactory(context.getSocketFactory())
+}
+
 fun main(args: Array<String>) {
     val gamesList: MutableList<Game> = mutableListOf()
-    val pagesCount = 86;
+    val pagesCount = 89;
+
+    enableSSLSocket();
 
     for (page in 1..pagesCount) {
 
-        val doc = Jsoup.connect("http://www.old-games.ru/catalog/?platform=1&sort=year&page=${page}").get()
+        val doc = Jsoup.connect("https://www.old-games.ru/catalog/?platform=1&sort=year&page=${page}").get()
         val games = doc.select("table.listtable tr")
 
         for (item in games) {
